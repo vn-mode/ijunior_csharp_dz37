c6b2daf9aa3b39a421d889564e1df7552ecf9b2a
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-07-05 15:02:38 +0300

    ijunior_csharp_dz37

---
+++ vn_mode_csharp_dz37/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] array1 = { "1", "2", "1" };
+        string[] array2 = { "3", "2" };
+
+        List<string> result = MergeWithoutDuplicates(array1, array2);
+
+        foreach (string s in result)
+        {
+            Console.WriteLine(s);
+        }
+    }
+
+    static List<string> MergeWithoutDuplicates(string[] array1, string[] array2)
+    {
+        List<string> mergedList = new List<string>();
+
+        AddUniqueItems(mergedList, array1);
+        AddUniqueItems(mergedList, array2);
+
+        return mergedList;
+    }
+
+    static void AddUniqueItems(List<string> list, string[] array)
+    {
+        foreach (string item in array)
+        {
+            if (!list.Contains(item))
+            {
+                list.Add(item);
+            }
+        }
+    }
+}
