33bcf50bdbc602cfba2894ca0e21be0c8e8ae650
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-07-06 18:40:52 +0300

    ijunior_csharp_dz37

--- vn_mode_csharp_dz37/Program.cs
+++ vn_mode_csharp_dz37/Program.cs
@@ -8,31 +8,44 @@ class Program
         string[] array1 = { "1", "2", "1" };
         string[] array2 = { "3", "2" };
 
-        List<string> result = MergeWithoutDuplicates(array1, array2);
+        List<string> result = new List<string>();
 
-        foreach (string s in result)
+        MergeWithoutDuplicates(array1, array2, result);
+
+        foreach (string item in result)
         {
-            Console.WriteLine(s);
+            Console.WriteLine(item);
         }
     }
 
-    static List<string> MergeWithoutDuplicates(string[] array1, string[] array2)
+    static void MergeWithoutDuplicates(string[] array1, string[] array2, List<string> mergedList)
     {
-        List<string> mergedList = new List<string>();
+        if (mergedList == null)
+        {
+            throw new ArgumentNullException(nameof(mergedList));
+        }
 
         AddUniqueItems(mergedList, array1);
         AddUniqueItems(mergedList, array2);
-
-        return mergedList;
     }
 
     static void AddUniqueItems(List<string> list, string[] array)
     {
-        foreach (string item in array)
+        if (list == null)
         {
-            if (!list.Contains(item))
+            throw new ArgumentNullException(nameof(list));
+        }
+
+        if (array == null)
+        {
+            throw new ArgumentNullException(nameof(array));
+        }
+
+        foreach (string element in array)
+        {
+            if (list.Contains(element) == false)
             {
-                list.Add(item);
+                list.Add(element);
             }
         }
     }
